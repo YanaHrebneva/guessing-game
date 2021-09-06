6ca6ce8bec0aae8f8d08149b855096ce6c2589b8
YanaHrebneva <ygrebneva96@gmail.com>
2021-09-06 16:50:32 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+       return this.middle = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
